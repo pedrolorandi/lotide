dbe09969def45e31ed760efe14fac1c764623cda
Pedro Lorandi <hello@pedrolorandi.com>
2022-10-25 15:07:49 -0400

    function middle working properly

--- middle.js
+++ middle.js
@@ -6,7 +6,7 @@ const eqArrays = function(array1, array2) {
   }
 
   return true;
-}
+};
 
 const assertArraysEqual = function(actual, expected) {
   let message = "";
@@ -16,9 +16,27 @@ const assertArraysEqual = function(actual, expected) {
   console.log(message);
 };
 
-let middleArray = [];
 const middle = function(array) {
+  let middleArray = [];
+  let halfIndex = Math.floor(array.length / 2);
 
-}
+  if (array.length <= 2) return middleArray;
+
+  if (array.length % 2 === 0) {
+    middleArray = array.slice(halfIndex - 1, halfIndex + 1);
+  } else {
+    middleArray.push(array[halfIndex]);
+  }
+
+  return middleArray;
+};
 
 // TEST CASE
+console.log(middle([1])); // => []
+console.log((middle([1, 2]))); // => []
+
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
