d7a4083ab8c58efcd02e378866523aac30b233c0
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-08 10:59:38 -0500

    Refactoring middleTest.js and tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,18 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', () => {
+  it("returns two elements for result.length", () => {
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("returns 'Lighthouse' for result[0]", () => {
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+
+  it("returns 'Labs' for result[1]", () => {
+    assert.strictEqual(result[1], "Labs");
+  });
+});
