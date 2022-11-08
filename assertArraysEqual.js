40a38e3f57859f25a3c9481c8ddef37ddfec040e
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-08 10:20:54 -0500

    Refactoring assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length)
-    return false;
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i])
-      return false;
-  }
-
-  return true;
-}
+const eqArrays = require('eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   let message = "";
@@ -18,3 +7,5 @@ const assertArraysEqual = function(actual, expected) {
   equal ? message = `✅✅✅ Assertion Passed: ${actual} === ${expected}` : message = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   console.log(message);
 };
+
+module.exports = assertArraysEqual;
