c0e4915a8fc70923979f96f5f3655d13af902266
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-14 11:26:26 -0500

    Added header comments to eqArraysTest and refactored the without function.

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,10 +1,12 @@
-const assertEqual = require('../assertEqual');
+const assert    = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  it("returns false for ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.deepEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
