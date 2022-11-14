9a46562e3a892ad843287771fe5123f68ff3f7ff
Pedro Lorandi <hello@pedrolorandi.com>
2022-11-14 10:58:08 -0500

    Added header comments to assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,6 @@
 const eqArrays = require('./eqArrays');
 
+// The function takes in 2 arrays and returns true if they are equal and false otherwise.
 const assertArraysEqual = function(actual, expected) {
   let message = "";
   let equal = eqArrays(actual, expected);
